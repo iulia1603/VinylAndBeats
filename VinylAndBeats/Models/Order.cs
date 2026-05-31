b39f3ae340d8c22f6fe76ec05aee1db30f3d8da0
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:37:35 +0300

    Update Order.cs

--- VinylAndBeats/Models/Order.cs
+++ VinylAndBeats/Models/Order.cs
@@ -1,6 +1,13 @@
-﻿namespace VinylAndBeats.Models
+﻿namespace VinylAndBeats.Models;
+
+public class Order : BaseEntity
 {
-    public class Order
-    {
-    }
+    public string BuyerId { get; set; } = string.Empty;
+    public ApplicationUser? Buyer { get; set; }
+
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+    public decimal Total { get; set; }
+
+    public List<OrderItem> Items { get; set; } = new();
 }
