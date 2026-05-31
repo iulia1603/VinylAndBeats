7f49a01e7aeb5f1b7d16a906db3ee8df26df146d
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:40:15 +0300

    Update AppDbContext.cs

--- VinylAndBeats/Data/AppDbContext.cs
+++ VinylAndBeats/Data/AppDbContext.cs
@@ -14,5 +14,6 @@ namespace VinylAndBeats.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+
     }
 }
