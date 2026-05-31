fefbdbd071a92e60497022769b59c89a6bff3695
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:37:00 +0300

    Update CartItem.cs

--- VinylAndBeats/Models/CartItem.cs
+++ VinylAndBeats/Models/CartItem.cs
@@ -1,6 +1,15 @@
-﻿namespace VinylAndBeats.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VinylAndBeats.Models;
+
+public class CartItem : BaseEntity
 {
-    public class CartItem
-    {
-    }
+    public int CartId { get; set; }
+    public Cart? Cart { get; set; }
+
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+
+    [Range(1, 100000)]
+    public int Quantity { get; set; }
 }
