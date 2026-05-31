80fd70816da72fea82734dc98a65624ca841b61b
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:38:27 +0300

    Update AppDbContext.cs

--- VinylAndBeats/Data/AppDbContext.cs
+++ VinylAndBeats/Data/AppDbContext.cs
@@ -12,7 +12,7 @@ namespace VinylAndBeats.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Order> Orders { get; set; }
-        //public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
