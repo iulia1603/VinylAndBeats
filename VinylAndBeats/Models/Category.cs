7ede7ee6d7747ef4bd1afb7240396c6160192082
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:32:21 +0300

    Update Category.cs

--- VinylAndBeats/Models/Category.cs
+++ VinylAndBeats/Models/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace VinylAndBeats.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VinylAndBeats.Models;
+
+public class Category : BaseEntity
 {
-    public class Category
-    {
-    }
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; } = string.Empty;
+
+    public List<Product> Products { get; set; } = new();
 }
