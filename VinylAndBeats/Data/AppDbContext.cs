3152d59f8e14e9cdbd8b778ba81802faabcca8e2
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:45:46 +0300

    Update AppDbContext.cs

--- VinylAndBeats/Data/AppDbContext.cs
+++ VinylAndBeats/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using VinylAndBeats.Models;
 
 namespace VinylAndBeats.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
@@ -14,6 +15,9 @@ namespace VinylAndBeats.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
-
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
     }
 }
