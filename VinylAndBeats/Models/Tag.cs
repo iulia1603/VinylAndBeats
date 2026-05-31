6617a513b14e0a1916c761ff660feefc99234e5f
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:34:06 +0300

    Update Tag.cs

--- VinylAndBeats/Models/Tag.cs
+++ VinylAndBeats/Models/Tag.cs
@@ -1,6 +1,12 @@
-﻿namespace VinylAndBeats.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VinylAndBeats.Models;
+
+public class Tag : BaseEntity
 {
-    public class Tag
-    {
-    }
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; } = string.Empty;
+
+    public List<Product> Products { get; set; } = new();
 }
