1afdb434562d31327c6f85f5fad682283eac702d
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:29:42 +0300

    AppUser

--- VinylAndBeats/Models/ApplicationUser.cs
+++ VinylAndBeats/Models/ApplicationUser.cs
@@ -7,4 +7,6 @@ public class ApplicationUser : IdentityUser
     public string FullName { get; set; } = string.Empty;
 
     public List<Product> Products { get; set; } = new();
+
+    public List<Order> Orders { get; set; } = new();
 }
