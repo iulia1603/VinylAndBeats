aa16b424fc5b0794dec4e20fe3ddcc1a460db135
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 19:35:18 +0300

    update ProductTag

--- VinylAndBeats/Models/ProductTag.cs
+++ VinylAndBeats/Models/ProductTag.cs
@@ -1,6 +1,13 @@
-﻿namespace VinylAndBeats.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VinylAndBeats.Models;
+
+[Table("ProductTags")]
+public class ProductTag
 {
-    public class ProductTag
-    {
-    }
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
+    public int TagId { get; set; }
+    public Tag Tag { get; set; } = null!;
 }
