77d39f7fc79b6ca1bff3bd30354f4cebc6962f39
iulia1603 <lia.georgescu.04@gmail.com>
2026-05-31 16:51:53 +0300

    commit1

---
+++ VinylAndBeats/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VinylAndBeats.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public decimal Total { get; set; }
+
+        // legatura cu utilizatorul - adaug la pasul cu Identity.
+
+        // relatie N-N cu Product, prin OrderItem
+        public List<OrderItem> OrderItems { get; set; } = new();
+    }
+}
---
+++ VinylAndBeats/Models/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace VinylAndBeats.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        // legatura catre Order
+        public int OrderId { get; set; }
+        public Order? Order { get; set; }
+
+        // legatura catre Product
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
